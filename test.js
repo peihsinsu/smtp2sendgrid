d8589097f4e44b388a9b70686553b5d965791f75
charles <chengwei@mitac.com.tw>
2015-02-03 12:59:16 +0000

    Update bin/smtp2sendgrid for attachments

--- test.js
+++ test.js
@@ -4,16 +4,17 @@ var mailer = require('nodeutil').mailutil;
 var ip = 'localhost';
 
 mailer.init(
-      {"smtpOptions":{"host":ip, "port": 3000}, "sender": "NO-REPLY <no-reply@micloud.tw>"}
+      {"smtpOptions":{"host":ip, "port": 25}, "sender": "NO-REPLY <no-reply@micloud.tw>"}
     );
 
 mailer.sendNodeMail({
 		from: "NO-REPLY <no-reply@micloud.tw>",
-    cc:["simon.su@gs.arecord.us"],
+    //cc:["simon.su@gs.arecord.us"],
+    bcc:["rock9030@gmail.com"],
     subject: "test123..." + new Date().toString(),
     html:"<h1>TEST123</h1>",
-    bcc:["simonsu.mail@gmail.com","simonsu.mail+cc@gmail.com"],
-    to:["simonsu@mitac.com.tw"] /*,
+    //bcc:["simonsu.mail@gmail.com","simonsu.mail+cc@gmail.com"],
+    to:["chengwei@mitac.com.tw"] ,
     attachments: [//see detail: https://github.com/andris9/Nodemailer#attachment-fields
        {   // utf-8 string as an attachment
             fileName: "text1.txt",
@@ -22,7 +23,7 @@ mailer.sendNodeMail({
         {   // binary buffer as an attachment
             fileName: "text2.txt",
             contents: new Buffer("hello world!","utf-8")
-        }] */
+        }] 
   },
   true, function(res){
     console.log(res);
