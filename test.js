b832a90c90812281b9dfcfc2608bf45fe0d1d236
Charles Fan <charles.fan@gigacloud.com.tw>
2016-01-05 11:32:54 +0800

    Change the email modules

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 var nu = require('nodeutil');
 
-var mailer = require('nodeutil').mailutil;
 var ip = 'localhost';
-
+//var mailer = require('nodeutil').mailutil;
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({"host":ip, "port": 25});
+/*
 mailer.init(
-      {"smtpOptions":{"host":ip, "port": 25}, "sender": "NO-REPLY <no-reply@micloud.tw>"}
+      {"smtpOptions":{"host":ip, "port": 2525}, "sender": "NO-REPLY <no-reply@micloud.tw>"}
     );
+*/
 
-mailer.sendNodeMail({
-		from: "NO-REPLY <no-reply@micloud.tw>",
+console.log(transporter);
+transporter.sendMail({
+		//from: "NO-REPLY <no-reply@micloud.tw>",
+		from: "micloud <developer@example.tw>",
     //cc:["simon.su@gs.arecord.us"],
-    bcc:["rock9030@gmail.com"],
-    subject: "test123..." + new Date().toString(),
+   // cc:["rock9030@gmail.com","cage+01@mitac.com.tw","simonsu@mitac.com.tw","sunnyhu@mitac.com.tw"],
+    subject: "micampus-test123..." + new Date().toString(),
     html:"<h1>TEST123</h1>",
     //bcc:["simonsu.mail@gmail.com","simonsu.mail+cc@gmail.com"],
+    //to:["cage+01@mitac.com.tw"] ,
     to:["chengwei@mitac.com.tw"] ,
     attachments: [//see detail: https://github.com/andris9/Nodemailer#attachment-fields
        {   // utf-8 string as an attachment
@@ -24,9 +30,5 @@ mailer.sendNodeMail({
             fileName: "text2.txt",
             contents: new Buffer("hello world!","utf-8")
         }] 
-  },
-  true, function(res){
-    console.log(res);
-  }
-);
+});
 
